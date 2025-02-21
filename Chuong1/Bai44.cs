cbca032ec33b9a23487ed444e42b4b56d01a782f
trqphuoc <phuocde308@gmail.com>
2025-02-21 22:46:14 +0700

    Bai44 lan 1

---
+++ Chuong1/Bai44.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Bai44
+{
+    static void Main()
+    {
+        
+    }
+
+    static int SumOfDigits(int n_331)
+    {
+        int sum_331 = 0;
+        while (n_331 > 0)
+        {
+            sum_331 += n_331 % 10;
+            n_331 /= 10;
+        }
+        return sum_331;
+    }
+}
+
