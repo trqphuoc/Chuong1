bb86473656ce2b2ce7e880595d6b39ff2643a3cd
trqphuoc <phuocde308@gmail.com>
2025-02-21 22:48:46 +0700

    Bai44 lan 2

--- Chuong1/Bai44.cs
+++ Chuong1/Bai44.cs
@@ -4,7 +4,11 @@ class Bai44
 {
     static void Main()
     {
-        
+        Console.Write("Nhập số nguyên dương n_331: ");
+        int n_331 = int.Parse(Console.ReadLine());
+
+        int sum_331 = SumOfDigits(n_331);
+        Console.WriteLine($"Tổng các chữ số của {n_331} là: {sum_331}");
     }
 
     static int SumOfDigits(int n_331)
