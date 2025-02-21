f8adf408257e976649722e8d7e89d3feb4bdb226
trqphuoc <phuocde308@gmail.com>
2025-02-21 19:45:55 +0700

    Bai11 lan 1

---
+++ Chuong1/Bai11.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Bai11
+{
+    static void Main()
+    {
+        // Nhập giá trị n_331 từ người dùng
+        Console.Write("Nhập giá trị n_331: ");
+        int n_331 = int.Parse(Console.ReadLine());
+
+        // Khởi tạo biến tổng
+        int sum_331 = 0;
+
+        
+    }
+}
