f9eab4655f87a8d41b9e8120e28dd9cf7eb545f5
trqphuoc <phuocde308@gmail.com>
2025-02-21 22:35:55 +0700

    Bai31 lan 1

---
+++ Chuong1/Bai31.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Bai31
+{
+    static void Main()
+    {
+        
+    }
+
+    static bool IsPrime(int n_331)
+    {
+        if (n_331 < 2) return false;
+        for (int i_331 = 2; i_331 * i_331 <= n_331; i_331++)
+        {
+            if (n_331 % i_331 == 0) return false;
+        }
+        return true;
+    }
+}
