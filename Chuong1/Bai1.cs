e223e3fcaca028d02e02bd53fa3c888e3bb55aa1
trqphuoc <phuocde308@gmail.com>
2025-02-21 18:03:51 +0700

    Lan 1

--- Chuong1/Bai1.cs
+++ Chuong1/Bai1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Chuong1
+class Bai1
 {
-    internal class Bai1
-    {
-    }
+    
 }
--- Chuong1/obj/Debug/net8.0/Chuong1.AssemblyInfo.cs
+++ Chuong1/obj/Debug/net8.0/Chuong1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chuong1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55813404a9e661e04620d312dbf2994e207bed51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chuong1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chuong1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
