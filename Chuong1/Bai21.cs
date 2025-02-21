7bc1a8cb62e0544d4473c0ed65d5bb4516c07573
trqphuoc <phuocde308@gmail.com>
2025-02-21 22:27:38 +0700

    Bai21 lan 2

--- Chuong1/Bai21.cs
+++ Chuong1/Bai21.cs
@@ -4,7 +4,11 @@ class Bai21
 {
     static void Main()
     {
-      
+        Console.Write("Nhập số nguyên dương n_331: ");
+        int n_331 = int.Parse(Console.ReadLine());
+
+        int sum_331 = SumOfDivisors(n_331);
+        Console.WriteLine($"Tổng các ước số của {n_331} là: {sum_331}");
     }
 
     static int SumOfDivisors(int n_331)
