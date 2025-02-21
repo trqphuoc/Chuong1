a783434c4e2bdadaa0857c548f297bccca3a994f
trqphuoc <phuocde308@gmail.com>
2025-02-21 22:39:08 +0700

    Bai31 lan 2

--- Chuong1/Bai31.cs
+++ Chuong1/Bai31.cs
@@ -4,7 +4,17 @@ class Bai31
 {
     static void Main()
     {
-        
+        Console.Write("Nhập số nguyên dương n_331: ");
+        int n_331 = int.Parse(Console.ReadLine());
+
+        if (IsPrime(n_331))
+        {
+            Console.WriteLine($"{n_331} là số nguyên tố.");
+        }
+        else
+        {
+            Console.WriteLine($"{n_331} không phải là số nguyên tố.");
+        }
     }
 
     static bool IsPrime(int n_331)
