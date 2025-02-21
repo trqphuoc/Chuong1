9bfaf4a676c5e7bee176cddf1893f5e9477f8f24
trqphuoc <phuocde308@gmail.com>
2025-02-21 22:54:24 +0700

    Bai52 lan 1

---
+++ Chuong1/Bai52.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Bai52
+{
+    static void Main()
+    {
+        
+    }
+
+    static int FindMinDigit(int n_331)
+    {
+        int minDigit_331 = 9;
+        while (n_331 > 0)
+        {
+            int digit_331 = n_331 % 10;
+            if (digit_331 < minDigit_331)
+            {
+                minDigit_331 = digit_331;
+            }
+            n_331 /= 10;
+        }
+        return minDigit_331;
+    }
+}
