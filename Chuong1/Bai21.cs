167d09bd0f3b6f0fe1940d063aa2264aff8bcfd8
trqphuoc <phuocde308@gmail.com>
2025-02-21 22:24:26 +0700

    Bai21 lan 1

---
+++ Chuong1/Bai21.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Bai21
+{
+    static void Main()
+    {
+      
+    }
+
+    static int SumOfDivisors(int n_331)
+    {
+        int sum_331 = 0;
+        for (int i_331 = 1; i_331 <= n_331; i_331++)
+        {
+            if (n_331 % i_331 == 0)
+            {
+                sum_331 += i_331;
+            }
+        }
+        return sum_331;
+    }
+}
